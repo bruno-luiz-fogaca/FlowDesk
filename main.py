a5f0a505b14ed0ff05edd8c0b26c77a06a660f0c
Bruno Luiz Fogaça <bruno.luiz.fogaca@gmail.com>
2026-08-16 15:52:52 -0300

    feat: Implementação do Menu e sua Respectiva Função

---
+++ main.py
@@ -0,0 +1,13 @@
+def menu():
+    while True:
+        print("----------------------")
+        print("BEM VINDO AO FLOW DESK")
+        print("----------------------")
+        print(" ")
+        print("[1] Cadastrar Cliente")
+        print("[2] Excluir Cliente")
+        print("[3] Chamar Próximo Cliente")
+        print("[4] Ver Lista de Cliente")
+        print("[5] Sair")
+
+        escolha = int(input("Opcção: "))
